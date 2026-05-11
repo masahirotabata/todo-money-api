13de51dc424e4d173ecdb7a0501ecc003c319817
MasahiroTabata <79088468+masahirotabata@users.noreply.github.com>
2026-05-12 00:05:25 +0900

    Fix Spring Security auto configuration imports

--- backend/src/main/java/com/example/todomoney/BackendApplication.java
+++ backend/src/main/java/com/example/todomoney/BackendApplication.java
@@ -2,8 +2,8 @@ package com.example.todomoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
     SecurityAutoConfiguration.class,
